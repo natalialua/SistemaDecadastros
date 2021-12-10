83ee9ace196425b62ab31c66f8277e6e933a6b2c
natalia <nataliamikaellen@hotmail.com>
2021-12-10 18:42:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//Programar Data do Evento 
+let DataAtual = "08/12/2021"
+let DataEvento = "20/12/2021"
+
+if (DataAtual < DataEvento) {
+
+ console.log("Data salva com sucesso!")
+} else {
+    console.log("Data incorreta!")
+}
+// Inserir idade 
+let idade = 18
+if (idade >= 18) {
+    // Idade permitida
+    console.log(" Idade salva com sucesso!")
+} else {
+    console.log("Permitido apenas para maiores de idade!")
+}
+// listar participantes
+let participantes= 19
+if (participantes < 100) {
+    //Numero permitido
+    console.log("Cadastrado com sucesso!")
+} else {
+    console.log("Total de participantes ultrapassa limite permitido")
+}
